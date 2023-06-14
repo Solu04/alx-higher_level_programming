a3d9c8a30e85e8ba1b4bcb96954e22b3545ba2bf
Solu04 <solutionresource15@gmail.com>
2023-06-14 04:33:47 -0700

    Fixing error

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-def mutiply_list_map(my_list=[], number=0):
-    return(list(map(lambda x: x * number, my_list)))
+def multiply_list_map(my_list=[], number=0):
+    if not my_list:
+        return []
+    else:
+        return list(map(lambda x: x * number, my_list))
